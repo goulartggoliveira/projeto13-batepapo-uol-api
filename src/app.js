f0df1c1ef1bc5d525899c7ed012ace0725311769
goulartggoliveira <gustavoggoliveira98@gmail.com>
2023-01-16 08:14:21 -0300

    feat: post message with fix

--- src/app.js
+++ src/app.js
@@ -15,6 +15,14 @@ const participantsSchema = joi.object({
   name: joi.string().required().min(2),
 });
 
+const messageSchema = joi.object({
+  from: joi.string().required(),
+  to: joi.string().required().min(2),
+  text: joi.string().required().min(1),
+  type: joi.string().required().valid("message", "private_message"),
+  time: joi.string(),
+});
+
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 
 try {
@@ -71,6 +79,34 @@ app.get("/participants", async (req, res) => {
       res.sendStatus(404);
     }
     res.send(participants);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
+app.post("/messages", async (req, res) => {
+  const { to, text, type } = req.body;
+  const { user } = req.headers;
+
+  const message = {
+    from: user,
+    to,
+    text,
+    type,
+    time: dayjs().format("HH:mm:ss"),
+  };
+  try {
+    const { error } = messageSchema.validate(message, { abortEarly: false });
+
+    if (error) {
+      const errors = error.details.map((detail) => detail.message);
+      return res.status(422).send(errors);
+    }
+
+    await messagesCollection.insertOne(message);
+
+    res.sendStatus(201);
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
