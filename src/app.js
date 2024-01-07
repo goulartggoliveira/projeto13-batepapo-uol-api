df87ab8e2ed2c5825bfd71152b5b110ad29f03e0
goulartggoliveira <gustavoggoliveira98@gmail.com>
2024-01-07 20:40:27 -0300

    POST and GET /messages

--- src/app.js
+++ src/app.js
@@ -16,6 +16,16 @@ dotenv.config();
 const participantSchema = joi.object({
   name: joi.string().required().min(3),
 });
+
+const messageSchema = joi.object({
+    from: joi.string().required(),
+    to: joi.string().required().min(1),
+    text: joi.string().required().min(1),
+    type: joi.string().required().valid("message", "private_message"),
+    time: joi.string(),
+});
+
+
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 try {
   await mongoClient.connect();
@@ -74,4 +84,60 @@ app.get("/participants", async (req, res) => {
     }
 });
 
+app.post("/messages", async (req, res) => {
+    const { to, text, type } = req.body;
+    const { user } = req.headers;
+
+    const message = {
+      from: user,
+      to,
+      text,
+      type,
+      time: dayjs().format("HH:mm:ss"),
+    };
+
+    try {
+      const { error } = messageSchema.validate(message, { abortEarly: false });
+
+      if (error) {
+        const errors = error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+      }
+      await messagesCollection.insertOne(message);
+
+      res.sendStatus(201);
+    } catch (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+});
+
+app.get("/messages", async (req, res) => {
+    const limit = Number(req.query.limit);
+    const { user } = req.headers;
+
+    try {
+      const messages = await messagesCollection
+        .find({
+          $or: [
+            { from: user },
+            { to: { $in: [user, "Todos"] } },
+            { type: "message" },
+          ],
+        })
+        .limit(limit)
+        .toArray();
+
+      if (messages.length === 0) {
+        return res.status(422).send("Limite com valor inválido");
+      }
+
+      res.send(messages);
+    } catch (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+});
+
 app.listen(5000, () => console.log("rodando perfeitamente 5000"));
+
