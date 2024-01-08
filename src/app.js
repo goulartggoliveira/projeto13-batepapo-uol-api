0504247904a564a894f4b78501e116a42b413ed9
goulartggoliveira <gustavoggoliveira98@gmail.com>
2024-01-08 10:24:45 -0300

    GET /message 422 err fix 2

--- src/app.js
+++ src/app.js
@@ -132,7 +132,11 @@ app.get("/messages", async (req, res) => {
         .limit(limit)
         .toArray();
 
-        if (limit !== undefined && (numberLimit <= 0 || isNaN(numberLimit))) return res.sendStatus(422)
+      if (messages.length === 0) {
+        return res.status(422).send("Limite com valor inválido");
+      }
+
+      if (limit !== undefined && (numberLimit <= 0 || isNaN(numberLimit))) return res.sendStatus(422)
 
       res.send(messages);
     } catch (err) {
